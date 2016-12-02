886d100038f2d71364786a7c19533412eae9c98f
Sebastian Emmer <sebastian.emmer@edu.campus02.at>
2016-12-02 15:55:44 +0100

    .

---
+++ uebung_bank_gehaltskonto/Bank_GH_Ausgabe.java
@@ -0,0 +1,11 @@
+
+public class Bank_GH_Ausgabe {
+
+	public static void main(String[] args) {
+		Gehaltskonto kunde1 = new Gehaltskonto("Max Maier", "AT1111222222", "FKGH3DK");
+		Gehaltskonto kunde2 = new Gehaltskonto("Susi Zuscher", "AT234234234", "FGKDKR33");
+		Gehaltskonto kunde3 = new Gehaltskonto("Ulrich Max", "AT02348423", "FDSKJ3");
+
+	}
+
+}
---
+++ uebung_bank_gehaltskonto/Gehaltskonto.java
@@ -0,0 +1,28 @@
+
+public class Gehaltskonto {
+	/*private String inhaber;
+	private String iban;
+	private String bic;
+	private double kontostand;
+
+	public Gehaltskonto(String inhaber, String iban, String bic) {
+
+	}
+
+	public void aufbuchen(double betrag) {
+		kontostand = kontostand + betrag;
+	}
+
+	public void abbuchen(double betrag) {
+		if (betrag > kontostand) {
+			System.out.println("Die Buchung kann nicht durchgeführt werden! Betrag höher als Kontostand!");
+		} else {
+			kontostand = kontostand - betrag;
+			System.out.println(kontostand);
+		}
+	}
+
+	public void getKontostand(double ks) {
+		System.out.println("Kontostand beträgt: " + this.kontostand);
+	}*/
+}
